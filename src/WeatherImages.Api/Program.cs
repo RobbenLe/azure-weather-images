6ba40de4b4c2ece4e23a78063b68a4ca23a34dee
Hung Ro Bben Le <144344546+RobbenLe@users.noreply.github.com>
2026-09-11 09:07:58 +0200

    Delete Trash in WeatherImages.Api

--- src/WeatherImages.Api/Program.cs
+++ src/WeatherImages.Api/Program.cs
@@ -26,27 +26,6 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
-
-
 app.MapPost("/api/images", async () =>
 {
     //Create jobid
@@ -58,7 +37,3 @@ app.MapPost("/api/images", async () =>
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
